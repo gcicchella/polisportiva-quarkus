1a2714c5a1498f5b981e9b8a28be8043681bc465
giuseppe <giuseppecicchella28@gmail.com>
2023-06-17 12:46:14 +0200

    fix: aggiunge email all'user

--- src/main/java/org/example/Model/Users.java
+++ src/main/java/org/example/Model/Users.java
@@ -14,6 +14,9 @@ public class Users {
     @Column(name = "nome")
     private String nome;
 
+    @Column(name = "email")
+    private String email;
+
     public Integer getId() {
         return id;
     }
@@ -28,5 +31,13 @@ public class Users {
 
     public void setNome(String nome) {
         this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 }
